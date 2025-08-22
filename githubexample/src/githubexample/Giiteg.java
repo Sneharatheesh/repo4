9a48329e68031615482a4bb0481ddd58e0a71f41
Sneha <snehastephanson@gmail.com>
2025-08-22 11:20:49 +0530

    commit

---
+++ githubexample/src/githubexample/Giiteg.java
@@ -0,0 +1,15 @@
+package githubexample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Giiteg {
+	public static void main(String[] args)
+	{		
+	System.out.println("Hi...Web page is launching with chrome");
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\USER\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.in");
+		}
+
+}
